a48447600b99b4d282b8c2aee411480dc09e3390
Chad Carbert <chadcarbert@me.com>
2014-12-24 15:35:50 -0800

    Formatted the time-block template markup to be a little cleaner. Added a helper model to contain system-wide helper functions. Made use of the formatted time helper to convert the ms into a readable HHMMSS format

--- app/components/timer-block.js
+++ app/components/timer-block.js
@@ -1,10 +1,18 @@
+import Ember from 'ember';
+import helper from '../models/helper';
+
 export default Ember.Component.extend({
 
 	active: null,
+	totalTime: null,
     
     status: function() {
         return this.get('active') ? 'ON' : 'OFF';
     }.property('active'),
+
+    formattedTime: function() {
+    	return helper.convertMsToHHMMSS(this.get('totalTime'));
+    }.property('totalTime'),
 
 	actions: {
 
